c7b3287581496d5b73595c37984dcb6568d77e6e
Brainmaniac <olof.jondelius@gmail.com>
2018-05-24 18:42:40 +0200

    last fixes website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
 
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-      <div class="container">
+      <div class="container-fluid">
         <a class="navbar-brand js-scroll-trigger" href="#page-top" id="logo">STIMPACK</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -35,18 +35,22 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#what-is-this">What is this?</a>
+              <a class="nav-link js-scroll-trigger" href="#what-is-this">About</a>
             </li>
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#gateways">Gateways</a>
             </li>
-
-            <!-- <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
-            </li> -->
-
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="https://www.youtube.com/watch?time_continue=1&v=hY7m5jjJ9mM" target="_blank">|</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="data.stimpack.io/register">Sign up</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="data.stimpack.io/login">Login</a>
             </li>
           </ul>
         </div>
@@ -94,34 +98,42 @@
       </div>
       <div class="container">
         <div class="row">
-          <div class="col-lg-3 col-md-6 text-center">
-            <div class="service-box mt-5 mx-auto">
-              <i class="fas fa-5x fa-cubes mb-3 color1 sr-icons"></i>
-              <h3 class="mb-3 color1 secondary-font font-600">Packs</h3>
-              <p class="text-muted mb-0 color2 secondary-font">Access ready-made packs</p>
+            <div class="col-lg-3 col-md-6 text-center">
+                <a href="data.stimpack.io" class="color1">
+                    <div class="service-box mt-5 mx-auto">
+                        <i class="fas fa-5x fa-cubes mb-3"></i>
+                        <h3 class="mb-3 secondary-font font-600">Packs</h3>
+                        <p class="mb-0 color2 secondary-font">Access ready-made packs</p>
+                    </div>
+                </a>
             </div>
-          </div>
-          <div class="col-lg-3 col-md-6 text-center">
-            <div class="service-box mt-5 mx-auto">
-              <i class="fab fa-5x fa-kickstarter mb-3 color1 sr-icons"></i>
-              <h3 class="mb-3 color1 secondary-font font-600">Kickstarter</h3>
-              <p class="text-muted mb-0 color2 secondary-font">Support our campaign at Kickstarter!</p>
+            <div class="col-lg-3 col-md-6 text-center">
+                <a href="kickstarter.com" class="color1" target="_blank">
+                    <div class="service-box mt-5 mx-auto">
+                        <i class="fab fa-5x fa-kickstarter mb-3"></i>
+                            <h3 class="mb-3 secondary-font font-600">Kickstarter</h3>
+                        <p class="mb-0 color2 secondary-font">Support our campaign at Kickstarter!</p>
+                    </div>
+                </a>
             </div>
-          </div>
-          <div class="col-lg-3 col-md-6 text-center">
-            <div class="service-box mt-5 mx-auto">
-              <i class="fab fa-5x fa-github mb-3 color1 sr-icons"></i>
-              <h3 class="mb-3 color1 secondary-font font-600">GitHub</h3>
-              <p class="text-muted mb-0 color2 secondary-font">Check out the repo</p>
+            <div class="col-lg-3 col-md-6 text-center">
+                <a href="https://github.com/stimpack-io/stimpack" class="color1" target="_blank">
+                    <div class="service-box mt-5 mx-auto">
+                        <i class="fab fa-5x fa-github mb-3"></i>
+                            <h3 class="mb-3 secondary-font font-600">GitHub</h3>
+                        <p class="mb-0 secondary-font color2">Check out the repo</p>
+                    </div>
+                </a>
             </div>
-          </div>
-          <div class="col-lg-3 col-md-6 text-center">
-            <div class="service-box mt-5 mx-auto">
-              <i class="fas fa-5x fa-globe mb-3 color1 sr-icons"></i>
-              <h3 class="mb-3 color1 secondary-font font-600">Join!</h3>
-              <p class="text-muted mb-0 color2 secondary-font">Register to be a part of this </p>
+            <div class="col-lg-3 col-md-6 text-center">
+                <a href="data.stimpack.io" class="color1">
+                    <div class="service-box mt-5 mx-auto">
+                        <i class="fas fa-5x fa-globe mb-3"></i>
+                            <h3 class="mb-3 secondary-font font-600">Join!</h3>
+                        <p class="mb-0 secondary-font color2">Register to be a part of the swarm</p>
+                    </div>
+                </a>
             </div>
-          </div>
         </div>
       </div>
     </section>
@@ -132,12 +144,12 @@
           <div class="col-lg-8 mx-auto text-center">
             <h2 class="section-heading headline">We are looking for collaborators!</h2>
             <hr class="my-4">
-            <p class="mb-5 color2 secondary-font">Send pull requests and then get in touch with us on this email and we'd happy for you to join us!</p>
+            <p class="mb-5 color2 secondary-font">Send pull requests and/or get in touch with us on this email and we'd be happy if you want to join us!</p>
           </div>
         </div>
         <div class="row">
           <div class="col-lg-12 mr-auto text-center">
-            <i class="far fa-5x fa-envelope mb-3 color1 sr-contact"></i>
+              <i class="far fa-5x fa-envelope mb-3 color1 sr-contact"></i>
             <p>
               <a class="secondary-font color2-hover-color1" href="mailto:hello@stimpack.io">hello@stimpack.io</a>
             </p>
